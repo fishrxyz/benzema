01d51509c13a5774db5bccb802b12078e4b0525d
fishrxyz <karim@fishr.xyz>
2020-11-29 15:34:59 +0100

    added type hints

--- benzema/decoder.py
+++ benzema/decoder.py
@@ -1,3 +1,4 @@
+from typing import Union, Optional
 from collections import OrderedDict
 from .constants import (
     DICT_IDENTIFIER, INT_IDENTIFIER,
@@ -13,18 +14,18 @@ class Decoder(object):
         its corresponding python representation.
     """
 
-    def __init__(self, payload):
+    def __init__(self, payload: bytes):
 
         if not isinstance(payload, bytes):
             _type = type(payload).__name__
             err = f'[error] Cannot decode payload. Expected bytes, got {_type}'
             raise TypeError(err)
 
-        self._payload = payload
-        self._index = 0
+        self._payload: bytes = payload
+        self._index: int = 0
 
     def decode(self):
-        char = self._opening_char
+        char: Optional[bytes] = self._opening_char
 
         if char == DICT_IDENTIFIER:
             self._index += 1
@@ -40,19 +41,19 @@ class Decoder(object):
         elif char in b'0123456789':
             return self._to_string()
 
-    def _to_dict(self):
+    def _to_dict(self) -> OrderedDict:
 
         # Note: an OrderedDict is being used in this case because we need
         # the 'info' dictionaries to be identical in order to produce the
         # correct sha1 hash that will be used to make a request to the tracker.
 
-        _dict = OrderedDict()
+        _dict: OrderedDict = OrderedDict()
 
         # loop through the payload until we reach 'e'
         while self._payload[self._index: self._index + 1] != EOT_IDENTIFIER:
             # decode whatever is at that index
-            key = self.decode()
-            value = self.decode()
+            key: bytes = self.decode()
+            value: bytes = self.decode()
 
             # populate the final list
             _dict[key] = value
@@ -64,12 +65,12 @@ class Decoder(object):
         self._index += 1
         return _dict
 
-    def _to_list(self):
-        _list = []
+    def _to_list(self) -> list:
+        _list: list = []
         # loop through the payload until we reach 'e'
         while self._payload[self._index:self._index+1] != EOT_IDENTIFIER:
             # decode whatever is at that index
-            element = self.decode()
+            element: bytes = self.decode()
 
             # populate the final list
             _list.append(element)
@@ -82,30 +83,30 @@ class Decoder(object):
         self._index += 1
         return _list
 
-    def _to_int(self):
+    def _to_int(self) -> int:
         # access EOT_IDENTIFIER (ie 'e')
-        end_token_index = self._payload.index(EOT_IDENTIFIER, self._index)
+        end_token_index: int = self._payload.index(EOT_IDENTIFIER, self._index)
 
         # read whatever is between 'i' and 'e' and cast that to an int
-        _int = int(self._payload[self._index:end_token_index])
+        _int: int = int(self._payload[self._index:end_token_index])
 
         # update index to start after EOT_IDENTIFIER
         self._index = end_token_index + 1
 
         return _int
 
-    def _to_string(self):
+    def _to_string(self) -> bytes:
         # get index of delimiter
-        str_delim_index = self._payload.index(STRING_DELIMITER, self._index)
+        str_delim_idx: int = self._payload.index(STRING_DELIMITER, self._index)
 
         # number of characters to read
-        chars_to_read = int(self._payload[self._index:str_delim_index])
+        chars_to_read: int = int(self._payload[self._index:str_delim_idx])
 
         # set the starting index at delimiter + 1 (ie the char after :)
-        start_index = str_delim_index + 1
+        start_index = str_delim_idx + 1
 
         # read `char_to_read` characters starting from delimiter index
-        data = self._payload[start_index:start_index + chars_to_read]
+        data: bytes = self._payload[start_index:start_index + chars_to_read]
 
         # update index to start at end of string
         self._index = start_index + chars_to_read
@@ -113,7 +114,7 @@ class Decoder(object):
         return data
 
     @property
-    def _opening_char(self):
+    def _opening_char(self) -> Union[bytes, None]:
         if self._index >= len(self._payload):
             # we've reached the end of the payload
             return None
--- benzema/encoder.py
+++ benzema/encoder.py
@@ -1,5 +1,9 @@
+from typing import Union
 from collections import OrderedDict
 from .constants import EOT_IDENTIFIER
+
+
+supported_types = Union[int, str, bytes, list, dict, OrderedDict]
 
 
 class Encoder(object):
@@ -8,10 +12,10 @@ class Encoder(object):
         Encodes payload to the bencode format.
     """
 
-    def __init__(self, payload):
+    def __init__(self, payload: supported_types):
         self._payload = payload
 
-    def encode(self):
+    def encode(self) -> supported_types:
         return self._encode_payload(self._payload)
 
     def _encode_payload(self, payload):
@@ -30,14 +34,14 @@ class Encoder(object):
             err = f'[error] Cannot encode object of type {data_type}'
             raise TypeError(err)
 
-    def _from_dict(self, _dict):
+    def _from_dict(self, _dict) -> bytearray:
         # we use a bytearray because it's impossible to
         # concatenate bytearray types to strings
 
-        encoded_dict = bytearray('d', 'utf-8')
+        encoded_dict: bytearray = bytearray('d', 'utf-8')
         for key, value in _dict.items():
-            k = self._encode_payload(key)
-            v = self._encode_payload(value)
+            k: Union[bytes, bytearray] = self._encode_payload(key)
+            v: Union[bytes, bytearray] = self._encode_payload(value)
 
             encoded_dict += k
             encoded_dict += v
@@ -45,11 +49,11 @@ class Encoder(object):
         encoded_dict += EOT_IDENTIFIER
         return encoded_dict
 
-    def _from_list(self, _list):
+    def _from_list(self, _list) -> bytearray:
         # we use a bytearray because it's impossible to
         # concatenate bytearray types to strings
 
-        encoded_list = bytearray('l', 'utf-8')
+        encoded_list: bytearray = bytearray('l', 'utf-8')
         encoded_list += b''.join([
             self._encode_payload(_list[idx])
             for idx in range(len(_list))
@@ -57,13 +61,13 @@ class Encoder(object):
         encoded_list += EOT_IDENTIFIER
         return encoded_list
 
-    def _from_int(self, _int):
+    def _from_int(self, _int) -> bytes:
         return bytes(f'i{str(_int)}e', 'utf-8')
 
-    def _from_string(self, string):
+    def _from_string(self, string) -> bytes:
         return bytes(f'{len(string)}:{string}', 'utf-8')
 
-    def _from_bytes(self, _bytes):
+    def _from_bytes(self, _bytes) -> bytearray:
         # copied from pieces.bencoding.Encoder
         result = bytearray()
         result += str.encode(str(len(_bytes)))
